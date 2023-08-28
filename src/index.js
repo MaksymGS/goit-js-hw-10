e216d5b79d676e2bee144e2fd5b76480847ab7ac
MaksymGS <130307586+MaksymGS@users.noreply.github.com>
2023-08-29 01:46:39 +0300

    added lib

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+import Notiflix from 'notiflix';
+
+// axios.defaults.headers.common["x-api-key"] = "live_hUpm0TPsmjbX4AC1QpGRotmyFij1HtEea16FWClWzwksRd4Bpv33ZC17i5Yzp1kM";
+console.log(axios.isCancel('something'))
+
+
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+
+new SlimSelect({
+  select: '#selectElement'
+})
